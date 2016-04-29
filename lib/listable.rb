866a8bbd96c3c73c196790ce839ea02619527988
possession4 <himebrittny@hotmail.com>
2016-04-28 21:26:21 -0400

    Continued refactoring. Updated listable.rb to include format date and format priority.

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,21 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+  def format_date(date1, date2 = "")
+  	if(date2 == "")
+    	date1 ? date1.strftime("%D") : "No due date"
+	else
+	    dates = @date1.strftime("%D") if @date1
+	    dates << " -- " + @date2.strftime("%D") if @date2
+	    dates = "N/A" if !dates
+	    return dates
+	end
+  end
+    def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 end
