045c6b3413db6f9d57966730c73f608548da1760
possession4 <himebrittny@hotmail.com>
2016-05-04 21:51:20 -0400

    Added error messages for add and delete methods.

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -7,12 +7,19 @@ class UdaciList
   end
   def add(type, description, options={})
     type = type.downcase
+    type_validate(type)
+
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
+
   end
   def delete(index)
-    @items.delete_at(index - 1)
+    if (index-1 > @items.count)
+      raise UdaciListErrors::IndexExceedsListSize, "Value #{index} is greater than the length of this list."
+    else
+      @items.delete_at(index - 1)
+    end
   end
   def all
     puts "-" * @title.length
@@ -22,4 +29,11 @@ class UdaciList
       puts "#{position + 1}) #{item.details}"
     end
   end
+
+  def type_validate(type)
+    unless (["todo","event","link"].include? type)
+      raise UdaciListErrors::InvalidItemType, "Invalid Item Type: #{type}"
+    end
+  end
+  
 end
