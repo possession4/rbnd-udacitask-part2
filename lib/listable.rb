fb8ca1e4ff25f696b7ff1804c02dea59268c79f2
possession4 <himebrittny@hotmail.com>
2016-05-07 09:58:56 -0400

    Added chronic and colorize gems. Also added Gemfile and Gemfile.lock

--- lib/listable.rb
+++ lib/listable.rb
@@ -13,9 +13,9 @@ module Listable
 	end
   end
     def format_priority(priority)
-    value = " ⇧" if priority == "high"
-    value = " ⇨" if priority == "medium"
-    value = " ⇩" if priority == "low"
+    value = " ⇧".colorize(:red) if priority == "high"
+    value = " ⇨".colorize(:yellow) if priority == "medium"
+    value = " ⇩".colorize(:blue) if priority == "low"
     value = "" if !priority
     return value
   end
